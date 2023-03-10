8227b662b77284499199e45e6481c87545c1158f
Mosaif Ali <mosaif.ali.39@github.com>
2023-03-10 18:25:53 +0530

    initial Commit

---
+++ PyCharm_Community_Edition_2021.3.1.lnk/PyCharm_Community_Edition_2021.3.1.lnk.ino
@@ -0,0 +1,9 @@
+void setup() {
+  Serial.begin(9600); // Initialize the Serial port with baud rate 9600
+}
+
+void loop() {
+  int output = random(-100, 100); // Generate a random number between 0 and 100
+  Serial.println(output); // Send the output number to the Serial port
+  delay(1000); // Wait for 1 second before sending the next number
+}
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from collections import deque
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import threading
+import serial
 
 
 class RealTimeGraphApp(tk.Tk):
@@ -19,7 +20,7 @@ class RealTimeGraphApp(tk.Tk):
         self.line, = self.ax.plot([], [], lw=2)
 
         # Create a deque to hold the data points
-        self.data = deque([0] * 50, maxlen=50)
+        self.data = deque([0] * 100, maxlen=100)
 
         # Create the canvas to display the plot
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)
@@ -47,7 +48,23 @@ class RealTimeGraphApp(tk.Tk):
         self.start_button.config(state=tk.NORMAL)
         self.stop_button.config(state=tk.DISABLED)
         # Initialize the animation thread
-        self.thread = None
+        self.thread_flag = False
+        self.read_thread_flag = False
+
+        # establishing serial communication
+
+    def read_from_serial(self):
+        # Read data from the serial port and add it to the deque
+        ser = serial.Serial('COM4', 9600)
+        while self.read_thread_flag:
+            try:
+                data = int(ser.readline().decode().strip())
+                self.data.append(data)
+                print(data)
+            except ValueError:
+                pass
+
+        ser.close()
 
     def start_animation(self):
         # Create a new thread for the animation process
@@ -55,26 +72,37 @@ class RealTimeGraphApp(tk.Tk):
         self.start_button.config(state=tk.DISABLED)
         self.stop_button.config(state=tk.NORMAL)
 
+        self.read_thread = threading.Thread(target=self.read_from_serial)
+        self.read_thread.daemon = True
+        self.read_thread_flag = True
+        self.read_thread.start()
+
         self.thread = threading.Thread(target=self.animation_thread)
+        self.thread_flag = True
         self.thread.daemon = True
         self.thread.start()
 
     def stop_animation(self):
         # Stop the animation thread
+        # self.read_thread.join()
+        #self.read_thread = None
+        self.read_thread_flag = False
+
+        # self.thread.join()
+        #self.thread = None
+        self.thread_flag = False
+
         self.start_button.config(state=tk.NORMAL)
         self.stop_button.config(state=tk.DISABLED)
 
-        self.thread = None
-
     def animation_thread(self):
-        while self.thread is not None:
+        while self.thread_flag:
             # Generate a new data point
-            data = random.randint(-100, 100)
+            data = self.data[-1]
 
             # Add the data to the deque
             self.data.append(data)
 
-            # Update the plot
             self.line.set_data(range(len(self.data)), self.data)
             self.ax.relim()
             self.ax.autoscale_view(True, True, True)
